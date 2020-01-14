0651e7ba03a8e5778bca24c6c4b4f354d0b9aec5
sho-nakao <familynakao0624@gmail.com>
2020-01-14 17:04:30 +0900

    collection_checkboxesの削除
    ・_form.html.hamlの書き換え
    ・グループの編集画面について

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def index
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
